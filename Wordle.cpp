c48781e2176d0872a98f7d36bf6673ca1e931d87
Saifelden <saifelden2006@gmail.com>
2026-05-07 23:17:50 +0300

    update code

--- Wordle.cpp
+++ Wordle.cpp
@@ -3,18 +3,17 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
 using namespace std;
 
 class player {
 public:
-	string username, email, password;
-	player() {};
+	string username;
+	string email;
+	string password;
+	player() = default ;
 
-	player(string name, string mail, string pass) {
-		username = name;
-		email = mail;
-		password = pass;
-	}
+	player(const string& name, const string& mail, const string& pass) :username(name), email(mail), password(pass) {};
 };
 
 class word {
@@ -27,7 +26,9 @@ class authentication {
 public:
 	vector<player> players;
 	authentication() {
-		string name, mail, pass;
+		string name;
+		string mail;
+		string pass;
 		ifstream input("players.txt");
 		player temp;
 		while (input >> name >> mail >> pass) {
@@ -37,20 +38,25 @@ public:
 			players.push_back(temp);
 		}
 	}
-	void signup(string name , string mail , string pass) {
+	void signup(string name , const string& mail ,const string& pass) {
+		for (int i = 0; i < players.size(); i++) {
+			if (name == players[i].username) {
+				cout << "\nUsername already exists, chose another username: ";
+				cin >> name;
+				i = -1;
+			}
+		}
 		player newplayer(name, mail, pass);
 		players.push_back(newplayer);
 		ofstream file("players.txt", ios::app);
 		file <<endl << name << endl<< mail << endl<< pass;
 		file.close();
-		cout << "new player hase created\n";
+		cout << "new player has been created\n";
 	}
-	bool login(string user, string pass) {
+	bool login(const string& user,const string& pass) {
 		for (int i = 0; i < players.size(); i++) {
-			if (user == players[i].username) {
-				if (pass == players[i].password) {
+			if (user == players[i].username&& pass == players[i].password) {
 					return true;
-				}
 			}
 		}
 		return false;
@@ -70,7 +76,7 @@ public:
 			temp.ID = ID;
 			temp.letters = letters;
 			listWord.push_back(temp);
-		};
+		}
 	}
 	word gettheword(int ID) {
 		for (int i = 0; i < listWord.size(); i++) {
@@ -86,9 +92,9 @@ public:
 
 class gameSession {
 public:
-	int sessionID, attempt = 0;
+	int sessionID;
 	word theWord;
-	gameSession() {};
+	gameSession() = default;
 
 	void getrandomword(vector<word>listWord) {
 		srand(time(0));
@@ -98,12 +104,11 @@ public:
 		theWord = listWord[randomIndex];
 	};
 	string checkLetters(string guess) {
-		attempt++;
 
 		string target = theWord.letters;
 		int n = target.size();
 
-		if (attempt == 1 && guess == "") {
+		if (guess == "") {
 			return string(n, '-');
 		}
 
@@ -148,6 +153,13 @@ int main()
 	int chose=0;
 	authentication system;
 	while (chose != 3) {
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6); 
+
+				cout << "======================================\n";
+				cout << "        Welcome To Wordle Game  \n";
+				cout << "======================================\n";
+				
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
 	cout << "1.sign-in\n";
 	cout << "2.sign-up\n";
 		cin >> chose;
@@ -159,9 +171,12 @@ int main()
 			cin >> name;
 			cout << "\nEnter password: ";
 			cin >> password;
+
 			if (system.login(name, password) == true) {
+
 				int x=1;
 				while (x==1) {
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
 					string guess;
 					gameSession thegame;
 					listOfWord words;
@@ -171,14 +186,12 @@ int main()
 					for (int i = 0; i < 6; i++) {
 						cout << "Enter your guess: ";
 						cin >> guess;
-						cout << thegame.checkLetters(guess) << endl;
-						if (thegame.checkLetters(guess) == "Winner") {
-							break;
-						}
-						else if (thegame.checkLetters(guess) == "Invalid") {
+						cout <<"The Word-> " << thegame.checkLetters(guess) << endl;
+						if (thegame.checkLetters(guess) == "Winner"|| thegame.checkLetters(guess) == "Invalid") {
 							break;
 						}
 					}
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
 					cout << "\nYou want to try again?";
 					cout << "\n1.Yes";
 					cout << "\n2.No\n";
@@ -200,14 +213,9 @@ int main()
 			cin >> mail;
 			cout << "\nEnter your password: ";
 			cin >> pass;
-			for (int i = 0; i < system.players.size(); i++) {
-				if (name == system.players[i].username) {
-					cout << "\nUsername already exists, chose another username: ";
-					cin >> name;
-					i = -1;
-				}
-			}
 			system.signup(name, mail, pass);
 		}
 	}
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
 }
